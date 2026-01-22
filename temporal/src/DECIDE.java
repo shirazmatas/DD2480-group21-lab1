e53fcbbb3bfb383fd949ce18c17e2ac993fce4bf
aliceco <aliceco@kth.se>
2026-01-22 12:14:51 +0100

    feat: add code skeleton #3
    Add code skeleton for decide.java for the main steps in program based on assignment specifications.
    
    Refs: #3

--- temporal/src/DECIDE.java
+++ temporal/src/DECIDE.java
@@ -1,7 +1,54 @@
-public class DECIDE{
+import java.awt.geom.*
 
-    public boolean DECIDE(){
+public class Decide{
+    public class Parameters {
+        double length1;
+        double radius1;
+        double epsilon;
+        double area1;
+        int qPts;
+        int quads;
+        double dist;
+        int nPts;
+        int kPts;
+        int aPts;
+        int bPts;
+        int cPts;
+        int dPts;
+        int ePts;
+        int fPts;
+        int gPts;
+        double length2;
+        double radius2;
+        double area2;
+    }
+
+    private enum Connectors {
+        AND,
+        OR,
+        NOTUSED
+    }
+
+    // LIC's
+
+
+    // Calculate matrixes
+    public boolean[] calculateCMV(){
+        return
+    }
+
+    public boolean[][] calculatePUM(){
+        return
+    }
+
+    public boolean[] calculateFUV(){
+        ret
+    }
+
+    public boolean decide(){
+
         return false;
     }
+
 
 }
