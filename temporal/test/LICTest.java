b4336812b0a9e0a0b4af7de3310a5c7eed986f80
lucas <lucaslu@kth.se>
2026-01-26 16:23:04 +0100

    [Feat] #8 Empty functions created for test cases for LIC functions.

---
+++ temporal/test/LICTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LICTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic0() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic4() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic6() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic7() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic8() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic9() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic10() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic11() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic12() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic13() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lic14() {
+    }
+}
