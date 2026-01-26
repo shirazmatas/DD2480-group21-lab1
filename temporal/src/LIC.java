30e8b50d528094d60f592091a3ea73091c37f098
eryngia <barnabas.tanczos@gmail.com>
2026-01-26 13:06:33 +0100

    feat: add LIC class skeleton to handle launch conditions
    Add LIC class skeleton containing unimplemented methods for all 15 LICs and some iteration helper methods.
    
    Refs: #3

---
+++ temporal/src/LIC.java
@@ -0,0 +1,116 @@
+public final class LIC {
+
+    private LIC() {}
+
+    /*
+    Predicates
+     */
+
+    @FunctionalInterface
+    private interface PairPredicate {
+        boolean test(Point a, Point b);
+    }
+
+    @FunctionalInterface
+    private interface TriplePredicate {
+        boolean test(Point a, Point b, Point c);
+    }
+
+    @FunctionalInterface
+    private interface NPredicate {
+        boolean test(Point[] points);
+    }
+
+    /*
+    LICs
+     */
+
+    public static boolean lic0(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic1(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic2(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic3(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic4(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic5(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic6(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic7(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic8(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic9(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic10(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic11(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic12(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic13(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean lic14(Point[] points, Parameters parameters) {
+        throw new UnsupportedOperationException();
+    }
+
+    /*
+    Iteration helpers
+     */
+
+    private static boolean anySeparatedN(Point[] points, int[] gaps, NPredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean anySeparatedTriple(Point[] points, int gap1, int gap2,  TriplePredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean anySeparatedPair(Point[] points, int gap, PairPredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean anyConsecutiveN(Point[] points, NPredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean anyConsecutiveTriple(Point[] points, TriplePredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean anyConsecutivePair(Point[] points, PairPredicate predicate) {
+        throw new UnsupportedOperationException();
+    }
+
+}
