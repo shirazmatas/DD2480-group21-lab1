c993c87417b57173aa9dfe00b35d4bdac7f69f78
Sheng Ye <oscarhuang35@gmail.com>
2026-01-29 03:10:10 +0100

    feat: implemented PUM
    Implemented calculatePUM and one complete test

--- temporal/src/Decide.java
+++ temporal/src/Decide.java
@@ -14,8 +14,22 @@ public class Decide {
         throw new UnsupportedOperationException();
     }
 
-    private boolean[][] calculatePUM(boolean[] cmv, Connector[][] lcm){
-        throw new UnsupportedOperationException();
+    public static boolean[][] calculatePUM(boolean[] cmv, Connector[][] lcm){
+        boolean[][] PUM = new boolean[cmv.length][cmv.length];
+        for(int i = 0; i<lcm.length ; i++){
+            for(int j = 0; j<lcm.length ; j++){
+                if(i==j){
+                    PUM[i][j] = cmv[i];
+                    continue;
+                }
+                switch (lcm[i][j]){
+                    case OR -> PUM[i][j] = cmv[i] || cmv[j];
+                    case AND -> PUM[i][j] = cmv[i] && cmv[j];
+                    case NOT_USED -> PUM[i][j] = true;
+                }
+            }
+        }
+        return PUM;
     }
 
     private boolean[] calculateFUV(boolean[][] pum, boolean[] puv) {
